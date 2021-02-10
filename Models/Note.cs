8d5aa6ffacd5fb70501b650f5161e2d88e92ce1d
Max <m.dutra2211@gmail.com>
2021-02-10 16:24:39 -0300

    Task APIRestfull asp.net core and sql server

---
+++ Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NotesAPP.Models
+{
+    public partial class Note
+    {
+        public int NotesId { get; set; }
+        public string NotesName { get; set; }
+        public string NoteDescription { get; set; }
+        public string Tag { get; set; }
+        public string NoteColor { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NotesAPP.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Notes = new HashSet<Note>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+
+        public virtual ICollection<Note> Notes { get; set; }
+    }
+}
